1e88ad6499710ae860c13772a0303c4f55a9a4d7
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2022-10-20 00:33:47 +0900

    [BOJ] / 스택 / 12분

--- leeseunghee/week3/10828.c
+++ leeseunghee/week3/10828.c
@@ -2,66 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-int cnt=0;
-int *arr, N;
+int cnt = 0;
+int* arr, N;
 
-void push(int num){
+void push(int num) {
 	arr[cnt] = num;
 	cnt++;
 }
 
-void pop(){
-	if(cnt == 0)
+void pop() {
+	if (cnt == 0)
 		printf("-1\n");
-	else{
+	else {
 		cnt--;
 		printf("%d\n", arr[cnt]);
 	}
 }
 
-void size(){
+void size() {
 	printf("%d\n", cnt);
 }
 
-void empty(){
+void empty() {
 	if (cnt == 0)
 		printf("1\n");
 	else
 		printf("0\n");
 }
 
-void top(){
-	if (cnt!=0){
+void top() {
+	if (cnt != 0) {
 		printf("%d\n", arr[cnt - 1]);
 	}
 	else
 		printf("-1\n");
 }
 
-int main(){
+int main() {
 	int num;
 	char stack[10];
 
 	scanf("%d", &N);
-	arr = (int *)malloc(sizeof(int) * N + 1);
+	arr = (int*)malloc(sizeof(int) * N + 1);
 
-	while(N-->0){
+	while (N-- > 0) {
 		scanf("%s", &stack);
 
-		if(!strcmp(stack, "push")){
+		if (!strcmp(stack, "push")) {
 			scanf("%d", &num);
 			push(num);
 		}
-		else if(!strcmp(stack, "pop")){
+		else if (!strcmp(stack, "pop")) {
 			pop();
 		}
-		else if(!strcmp(stack, "size")){
+		else if (!strcmp(stack, "size")) {
 			size();
 		}
-		else if(!strcmp(stack, "empty")){
+		else if (!strcmp(stack, "empty")) {
 			empty();
 		}
-		else if(!strcmp(stack, "top")){
+		else if (!strcmp(stack, "top")) {
 			top();
 		}
 	}
