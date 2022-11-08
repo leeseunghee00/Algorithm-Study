3f748836069047399c2cef2fdd0dec9d3efcd10b
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2022-11-09 02:18:08 +0900

    [BOJ] / 나이순 정렬 / 1시간 30분

---
+++ leeseunghee/week4/10814.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int age;
+	char name[101];
+} Member;
+
+int compare(Member* a, Member* b) {
+	return (a->age > b->age ? 1 : (a->age < b->age ? -1 : 0));
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	Member* m;
+	m = (Member*)malloc(sizeof(Member) * n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %s", &m[i].age, m[i].name);
+	}
+
+	qsort(m, n, sizeof(Member), compare);
+
+	for (int i = 0; i < n; i++) {
+		printf("%d %s\n", m[i].age, m[i].name);
+	}
+
+	return 0;
+}
