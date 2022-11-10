87b3190d7c204d6252e9cb8a1ec2bb32589ad2dc
Seunghee Lee <87460638+leeseunghee00@users.noreply.github.com>
2022-11-10 17:28:52 +0900

    [BOJ] / 시리얼 번호 / 실패

---
+++ leeseunghee/week4/1431.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int sum(char *s){
+	int result = 0;
+
+	for(int i=0; i<strlen(s); i++){
+		if(s[i] >= '0' && s[i] <= '9')
+			result += s[i] - '0';
+	}
+
+	return result;
+}
+
+int compare(const void *first, const void *second){
+	char *a = *(char **)first;
+	char *b = *(char **)second;
+
+	if(strlen(a) != strlen(b)){
+		return strlen(a) > strlen(b);
+	}
+	else{
+		int asum = sum(a);
+		int bsum = sum(b);
+
+		if(asum != bsum)
+			return asum > bsum;
+		else
+			return a > b;
+	}
+}
+
+int main(){
+	int n;
+	char **str;
+	
+	scanf("%d", &n);
+
+	str = (char **)malloc(sizeof(char *) * n);
+
+	for(int i=0; i<n; i++){
+		str[i] = (char *)malloc(sizeof(char) * 50);
+		scanf("%s", str[i]);
+	}
+
+	qsort((void*)str, n, sizeof(str[0]), compare);
+
+	for(int i=0; i<n; i++){
+		printf("%s\n", str[i]);
+	}
+}
